50391f6a23439089d4b837c67b5d7fa9e9fb6703
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2022-08-05 18:29:47 +0200

    Create Coc.java

---
+++ net/gabriele/clashofcraft/client/Coc.java
@@ -0,0 +1,84 @@
+package net.gabriele.clashofcraft.client;
+
+import net.gabriele.clashofcraft.client.gamestate.LandMain;
+import net.gabriele.clashofcraft.client.gamestate.State;
+import org.jsfml.graphics.RenderWindow;
+import org.jsfml.graphics.Texture;
+import org.jsfml.system.Vector2f;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Coc {
+    private RenderWindow window;
+    private Vector2f mouseX_Y;
+    private String title = "Clash";
+    public Stack<State> states = new Stack<>();
+    public static HashMap<String, Texture> itemtextures = new HashMap<>();
+    public static HashMap<String, Texture> buildingtextures = new HashMap<>();
+    public Coc()
+    {
+        init();
+        states.add(new LandMain());
+    }
+    public void init()
+    {
+        initItemTextures();
+        initBuildingTextures();
+        initWindow();
+    }
+    private void initWindow() {
+        window = new RenderWindow(new VideoMode(1280,720),title);
+    }
+    public boolean RunningNow()
+    {
+        return window.isOpen();
+    }
+    public void update()
+    {
+        for(Event event : window.pollEvents())
+        {
+            switch(event.type)
+            {
+                case CLOSED:
+                    window.close();
+                    break;
+            }
+        }
+        states.lastElement().update();
+    }
+     public void updatedt()
+     {
+
+     }
+    public void render()
+    {
+        window.clear();
+        states.lastElement().render(window);
+        window.display();
+    }
+    private void initItemTextures()
+    {
+        Texture cobb = new Texture();
+        try {
+            cobb.loadFromFile(Paths.get("Assets/graphics/items/cobblestone.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        itemtextures.put("cobblestone",cobb);
+    }
+    private void initBuildingTextures()
+    {
+        Texture thlv1 = new Texture();
+        try {
+            thlv1.loadFromFile(Paths.get("Assets/graphics/buildings/distict_hall.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        buildingtextures.put("t_hall",thlv1);
+    }
+}
