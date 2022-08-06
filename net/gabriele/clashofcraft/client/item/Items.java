bce2f343903528bbc927c3e6244813c6be7197bd
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2022-08-06 16:13:48 +0200

    Create Items.java

---
+++ net/gabriele/clashofcraft/client/item/Items.java
@@ -0,0 +1,9 @@
+package net.gabriele.clashofcraft.client.item;
+
+import net.gabriele.clashofcraft.client.Coc;
+
+public class Items {
+    public static final Item NO_ITEM = new Item(Coc.itemtextures.get("no_item"),0,0);
+
+    public static final Item COBBLESTONE = new Item(Coc.itemtextures.get("cobblestone"), 1,64);
+}
