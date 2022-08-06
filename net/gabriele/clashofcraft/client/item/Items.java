4d0a93a956f33e503016fd66d8459521e24c8e05
Gabriele007xx <97365913+Gabriele007xx@users.noreply.github.com>
2022-08-06 16:13:36 +0200

    Delete Items.java

--- net/gabriele/clashofcraft/client/item/Items.java
+++
@@ -1,8 +0,0 @@
-package net.gabriele.clashofcraft.client.item;
-
-import net.gabriele.clashofcraft.client.Coc;
-
-public class Items {
-
-    public static final Item COBBLESTONE = new Item(Coc.itemtextures.get("cobblestone"), 0,64);
-}
